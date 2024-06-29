e951a7cb55df1fad1ac61f527b68ea0485b5fc20
Hatim Selmun <selmun13@gmail.com>
2024-06-29 23:42:34 +0100

    pagination

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -4,6 +4,7 @@ import base64 from 'base-64';
 import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
 import process from 'process';
+import { ObjectId } from 'mongodb';
 
 export default class FilesController {
     static async postUpload(req, res) {
@@ -72,12 +73,119 @@ export default class FilesController {
                 file._id = result.insertedId;
                 const fileToReturn = { ...file, id: file._id };
                 delete fileToReturn._id;
-                
+                delete fileToReturn.localPath;
+
                 res.status(201).send(fileToReturn);
             }
         } catch(err) {
             console.error(err);
             res.status(401).send({"error":"Unauthorized"});
+        }
+    }
+
+    static async getShow(req, res) {
+        const { id } = req.params;
+        // console.log(req.params);
+        // console.log(id);
+
+        const token = req.header('X-Token');
+        const key = `auth_${token}`;
+        try {
+            const userId = await redisClient.get(key);
+            if (!userId) {
+                return res.status(401).send({"error":"Unauthorized"});
+            }
+
+            const file = await dbClient.filesCollection.findOne({
+                _id: new ObjectId(id),
+                userId
+            })
+            if (!file) {
+                return res.status(404).send({"error":"Not found"});
+            }
+
+            const { _id, ...rest } = file;
+            const responseFile = { id: _id, ...rest };
+
+            delete responseFile.localPath;
+            res.status(200).send(responseFile);
+        } catch(err) {
+            console.log(err);
+            res.status(401).send({"error":"Unauthorized"});
+        }
+    }
+
+    // static async getIndex(req, res) {
+    //     const { parentId, page } = req.query;
+    //     const token = req.header('X-Token');
+    //     const key = `auth_${token}`;
+    //     try {
+    //         const userId = await redisClient.get(key);
+    //         if (!userId) {
+    //             return res.status(401).send({"error":"Unauthorized"});
+    //         }
+
+    //         let filesCursor = '';
+    //         if (parentId) {
+    //             if (page) {
+    //                 filesCursor = await dbClient.filesCollection.find({
+    //                     parentId
+    //                 }).skip(page * 20).limit(20);
+    //             } else {
+    //                 filesCursor = await dbClient.filesCollection.find({
+    //                     parentId
+    //                 }).limit(20);
+    //             }
+    //         } else if (page) {
+    //             filesCursor = await dbClient.filesCollection.find().skip(page * 20).limit(20);
+    //         } else {
+    //             filesCursor = await dbClient.filesCollection.find().limit(20);
+    //         }
+    //         const files = await filesCursor.toArray();
+            
+    //         const resultFiles = files.map(file => {
+    //             const { _id, ...rest } = file;
+    //             return { id: _id, ...rest };
+    //         });
+
+    //         res.status(200).send(resultFiles);
+    //     } catch(err) {
+    //         console.log(err);
+    //         res.status(401).send({"error":"Unauthorized"});
+    //     }
+    // }
+    static async getIndex(req, res) {
+        const { parentId, page } = req.query;
+        const token = req.header('X-Token');
+        const key = `auth_${token}`;
+        try {
+            const userId = await redisClient.get(key);
+            if (!userId) {
+                return res.status(401).send({ "error": "Unauthorized" });
+            }
+    
+            const pageNumber = parseInt(page, 10) || 0;
+            const query = { userId };
+            if (parentId) {
+                query.parentId = parentId;
+            }
+    
+            const filesCursor = await dbClient.filesCollection.find(query)
+                .skip(pageNumber * 20)
+                .limit(20);
+    
+            const files = await filesCursor.toArray();
+    
+            const resultFiles = files.map(file => {
+                const { _id, ...rest } = file;
+                return { id: _id, ...rest };
+            });
+    
+            delete resultFiles.localPath;
+            res.status(200).send(resultFiles);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send({ "error": "Internal Server Error" });
         }
     }
 }
